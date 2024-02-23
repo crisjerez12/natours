32593b21676b7c5c16a7030e7cc179efbefd707e
Crisanto Jerez <crisjerez104@gmail.com>
2024-02-23 21:17:17 +0800

    added an Error handling

--- app.js
+++ app.js
@@ -2,7 +2,8 @@ const express = require('express');
 const morgan = require('morgan');
 const tourRouter = require('./routes/tourRoutes');
 const userRouter = require('./routes/userRoutes');
-
+const AppError = require('./utils/appError');
+const globalErrorHandler = require('./controllers/errorController');
 const app = express();
 
 if (process.env.NODE_ENV === 'development') {
@@ -19,9 +20,9 @@ app.use((req, res, next) => {
 app.use('/api/v1/tours', tourRouter);
 app.use('/api/v1/users', userRouter);
 app.all('*', (req, res, next) => {
-  res.status(404).json({
-    status: 'Fail',
-    message: `Can't find ${req.originalUrl} on this server!`,
-  });
+  next(new AppError(`Can't find ${req.originalUrl} on this server!`));
 });
+
+app.use(globalErrorHandler);
+
 module.exports = app;
